84983321b4c49ac31607c4c3d65926a5fff9cf6b
tbousque <tbousque@student.42.fr>
2022-06-11 18:42:52 +0200

    clean

--- fdf_init.c
+++ fdf_init.c
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 11:04:36 by tbousque          #+#    #+#             */
-/*   Updated: 2022/06/11 17:30:48 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/06/11 18:37:28 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,7 @@ int	main(int argc, char **argv)
 	mlx.map->transform = mat4x4_identity();
 	if (mlx.map == NULL)
 		return (EXIT_FAILURE);
-	mlx.proj = set_projection_matrix(0.1f, 1000.0f, 90.f, (float)mlx.img.y / (float)mlx.img.x);
-	mlx_do_key_autorepeaton(mlx.mlx_ptr);
-	//mlx_key_hook(mlx.win_ptr, &key_event, &mlx);
+	mlx.proj = set_all_proj(0.1f, 1000.0f, 90.f, (float)mlx.img.y / (float)mlx.img.x);
 	mlx_hook(mlx.win_ptr, 2, 1L<<0, &key_event, &mlx);
 	mlx_hook(mlx.win_ptr, 6, 1L<<6, &mouse_event, &mlx);
 	mlx_hook(mlx.win_ptr, 17, 1L<<0, &mlx_context_free, &mlx);
