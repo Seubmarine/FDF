454ccbd9c677fe322784ad4843b20bb417f21e7c
tbousque <tbousque@student.42.fr>
2022-06-25 20:04:41 +0200

    norm GOOD

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 17:52:16 by tbousque          #+#    #+#             */
-/*   Updated: 2022/06/25 19:22:00 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/06/25 20:04:08 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,36 +34,54 @@ void	image_clear(t_img img)
 	ft_memset(img.buffer, 0, img.x * sizeof(unsigned int) * img.y);
 }
 
-unsigned int	rgb(char alpha, char red, char green, char blue)
+// unsigned int	rgb(char alpha, char red, char green, char blue)
+// {
+// 	return (alpha << 24 | red << 16 | green << 8 | blue);
+// }
+
+t_brasenham_helper	draw_helper(t_vec2di a, t_vec2di b)
 {
-	return (alpha << 24 | red << 16 | green << 8 | blue);
+	t_brasenham_helper	x;
+
+	x.dx = ft_abs(b.x - a.x);
+	x.sx = -1;
+	if (a.x < b.x)
+		x.sx = 1;
+	x.dy = ft_abs(b.y - a.y);
+	x.sy = -1;
+	if (a.y < b.y)
+		x.sy = 1;
+	x.err = -x.dy;
+	if (x.dx > x.dy)
+		x.err = x.dx;
+	x.err /= 2;
+	return (x);
 }
 
 void	image_draw_line(t_img img, t_vec2di a, t_vec2di b)
 {
-	int	dx = abs(b.x - a.x), sx = a.x < b.x ? 1 : -1;
-	int	dy = abs(b.y - a.y), sy = a.y < b.y ? 1 : -1; 
-	int	err = (dx > dy ? dx : -dy) / 2, e2;
+	t_brasenham_helper	t;
+	int					e2;
 
+	t = draw_helper(a, b);
 	while (1)
 	{
 		image_put_pixel(img, a.x, a.y, 0xFF34EBE5);
 		if (a.x == b.x && a.y == b.y)
 			break ;
-		e2 = err;
-		if (e2 > -dx)
+		e2 = t.err;
+		if (e2 > -t.dx)
 		{
-			err -= dy;
-			a.x += sx;
+			t.err -= t.dy;
+			a.x += t.sx;
 		}
-		if (e2 < dy)
+		if (e2 < t.dy)
 		{
-			err += dx;
-			a.y += sy;
+			t.err += t.dx;
+			a.y += t.sy;
 		}
 	}
 }
-
 /*
 //Bresenham algorithm 
 void	image_draw_line(t_img img, int x0, int y0, int x1, int y1, int rgb) {
@@ -91,52 +109,6 @@ void	image_draw_line(t_img img, int x0, int y0, int x1, int y1, int rgb) {
 	}
 }
 */
+
+
 //0xFF34EBE5
-
-// #include <stdbool.h>
-// void image_draw_line(t_img surface, int x, int y, int x2, int y2, int rgb) {
-//    	bool yLonger=false;
-// 	int shortLen=y2-y;
-// 	int longLen=x2-x;
-// 	if (abs(shortLen)>abs(longLen)) {
-// 		int swap=shortLen;
-// 		shortLen=longLen;
-// 		longLen=swap;				
-// 		yLonger=true;
-// 	}
-// 	int decInc;
-// 	if (longLen==0) decInc=0;
-// 	else decInc = (shortLen << 16) / longLen;
-
-// 	if (yLonger) {
-// 		if (longLen>0) {
-// 			longLen+=y;
-// 			for (int j=0x8000+(x<<16);y<=longLen;++y) {
-// 				image_put_pixel(surface,j >> 16,y, rgb);	
-// 				j+=decInc;
-// 			}
-// 			return;
-// 		}
-// 		longLen+=y;
-// 		for (int j=0x8000+(x<<16);y>=longLen;--y) {
-// 			image_put_pixel(surface,j >> 16,y, rgb);	
-// 			j-=decInc;
-// 		}
-// 		return;	
-// 	}
-
-// 	if (longLen>0) {
-// 		longLen+=x;
-// 		for (int j=0x8000+(y<<16);x<=longLen;++x) {
-// 			image_put_pixel(surface,x,j >> 16, rgb);
-// 			j+=decInc;
-// 		}
-// 		return;
-// 	}
-// 	longLen+=x;
-// 	for (int j=0x8000+(y<<16);x>=longLen;--x) {
-// 		image_put_pixel(surface,x,j >> 16, rgb);
-// 		j-=decInc;
-// 	}
-
-// }
--- mlx_image.h
+++ mlx_image.h
@@ -6,7 +6,7 @@
 /*   By: tbousque <tbousque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 05:12:18 by tbousque          #+#    #+#             */
-/*   Updated: 2022/06/25 19:26:45 by tbousque         ###   ########.fr       */
+/*   Updated: 2022/06/25 19:46:21 by tbousque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,16 @@ typedef struct s_img
 	int		x;
 	int		y;
 }	t_img;
+
+typedef struct s_brasenham_helper
+{
+	int	dx;
+	int	dy;
+	int	sx;
+	int	sy;
+	int	err;
+	int	e2;
+}	t_brasenham_helper;
 
 void	image_put_pixel(t_img img, int x, int y, unsigned int rgb);
 t_img	image_create(void *mlx_ptr, int size_x, int size_y);
